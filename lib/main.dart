925d118c3a597ff56280e897e86145a5b3baf7ac
HooiErn <d200217c.sc.edu.my>
2023-04-06 00:49:59 +0800

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _StoryPageState extends State<StoryPage> {
         constraints: BoxConstraints.expand(),
         decoration: BoxDecoration(
             image: DecorationImage(
-            image: AssetImage("images/Star.jpg"),
+            image: AssetImage("images/Star.jpg"),// background image assets
             fit: BoxFit.cover,
             ),
         ),
@@ -40,13 +40,20 @@ class _StoryPageState extends State<StoryPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-            Text(
-            "Adventure Game",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-            ),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.landscape, size: 30),
+              SizedBox(width: 10), // Add some space between the icon and text
+              Text(
+                "Adventure Game",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
               Expanded(
                 flex: 12,
@@ -66,7 +73,7 @@ class _StoryPageState extends State<StoryPage> {
                     // Choice 1 made by user
                     setState(() {
                       storyBrain.nextStory(1);
-                      print("Button clicked");
+                      print("Button 1 clicked");
                     });
                   },
                   style: ButtonStyle(
@@ -100,7 +107,7 @@ class _StoryPageState extends State<StoryPage> {
                       // Choice 2 made by user
                       setState(() {
                         storyBrain.nextStory(2);
-                        print("Button clicked");
+                        print("Button 2 clicked");
                       });
                     },
                     style: ButtonStyle(
@@ -129,13 +136,4 @@ class _StoryPageState extends State<StoryPage> {
   }
 }
 
-//TODO: Step 1 - Find a background image, add the background image into the images directory, then add the background image to this Container.
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
-//TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-//TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-//TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-//TODO: Step 26 - Use a Flutter Visibility Widget to wrap this TextButton.
-//TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps.
+
